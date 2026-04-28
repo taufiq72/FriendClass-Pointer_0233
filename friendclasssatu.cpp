97df76cabe02f73f946ae384f2d75e33196f77da
taufiq72 <taufiqq1207@gmail.com>
2026-04-28 18:44:01 +0700

    membuat class siswa

--- friendclasssatu.cpp
+++ friendclasssatu.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 class siswa;
-lass orang
+class orang
 {
 private:
     string nama;
@@ -11,4 +11,14 @@ private:
 public:
     void setNama(string pNama);
     friend class siswa;
+};
+
+class siswa
+{
+private:
+    int id;
+
+public:
+    void setId(int pId);
+    void displayAll(orang &a);
 };
